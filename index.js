f768b95e4596b8a638b0c44b373720e83d38ef68
oluwasegun michael <alabioluwasegun8@gmail.com>
2022-05-26 17:26:58 +0100

    html done

---
+++ index.js
@@ -0,0 +1,15 @@
+const images = [
+    '<img src="car1.jfif" alt="">',
+    '<img src="car2.jfif" alt="">',
+    '<img src="car5.jfif" alt="">',
+    '<img src="car3.jfif" alt="">',
+    '<img src="car6.jfif" alt="">',
+    '<img src="car7.jfif" alt="">',
+    '<img src="car8.jfif" alt="">',
+    '<img src="car9.jfif" alt="">',
+    '<img src="car10.jfif" alt="">'
+]
+
+function displayCars(){
+    Math.flo
+}
